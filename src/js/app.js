9da245a7a612794de43dd6644064bd080a2bfa75
sebastianlopezs <135356217+sebastianlopezs@users.noreply.github.com>
2023-09-10 20:27:29 +0000

    mejor tarde que nunca

--- src/js/app.js
+++ src/js/app.js
@@ -33,17 +33,29 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+          <h1> ${variables.name == null ? "Nombre" : variables.name} ${
+    variables.lastname == null ? "Apellido" : variables.lastname
+  }</h1>   
+          <h2>${variables.role == null ? "Role" : variables.role}</h2>
+          <h3>${variables.city == null ? "City, " : variables.city}${
+    variables.country == null ? "Country" : variables.country
+  }</h3>
+                  <ul class="${variables.socialMediaPosition}">
+                    <li><a href="https://twitter.com/${
+                      variables.twitter == null ? "" : variables.twitter
+                    }"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="https://github.com/${
+                      variables.github == null ? "" : variables.github
+                    }"><i class="fab fa-github"></i></a></li>
+                    <li><a href="https://linkedin.com${
+                      variables.linkedin == null ? "" : variables.linkedin
+                    }/"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="https://instagram.com/${
+                      variables.instagram == null ? "" : variables.instagram
+                    }"><i class="fab fa-instagram"></i></a></li>
+                  </ul>
+                </div>
+            `;
 }
 
 /**
@@ -58,7 +70,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "position-right",
     // social media usernames
     twitter: null,
     github: "alesanchezr",
